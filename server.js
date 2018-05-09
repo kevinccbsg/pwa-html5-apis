e58d5614a542571dbbfde2675f6febc9291c3735
kevinccbsg <kevinccbsg@gmail.com>
2018-05-09 14:15:14 -0400

    Fixed typo on Listening message

--- server.js
+++ server.js
@@ -2,8 +2,6 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const formidable = require('formidable');
-const fs = require('fs');
 
 const app = express();
 
@@ -15,25 +13,4 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-
-app.post('/api', (req, res) => {
-  const form = new formidable.IncomingForm();
-  form.parse(req, function(err, fields, files) {
-    if (!fields.photo) {
-      return res.status(400).json({
-        error: '400 Bad Request'
-      });
-    }
-    let photoString = fields.photo;
-    if (fields.photo.includes('image/png;base64,')) {
-      photoString = fields.photo.replace('data:image/png;base64,', '');
-    }
-    console.log(photoString);
-    fs.writeFile('recognition.png', new Buffer(photoString, 'base64'), function(err) {
-      console.log(err);
-    });
-    res.send('ok');
-  });
-});
-
-app.listen(3000, () => console.log('Listening 300'));
+app.listen(3000, () => console.log('Listening 3000'));
